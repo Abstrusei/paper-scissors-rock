2b3de9e344e314d573ffb8beb51d0d92b424a2c6
Sean Marshall <smarshallean@gmail.com>
2022-01-25 14:58:02 +1000

    added some basic code

--- psr.js
+++ psr.js
@@ -1,1 +1,16 @@
 // Computer play function (randomly returns rock, paper or scissors)
+function computerPlay() {
+    let plays = ["paper", "scissors", "rock"];
+    let computersPlay = plays[Math.floor(Math.random() * actions.length)];
+    return computersPlay;
+}
+
+function playRound(playerSelection, computerSelection) {
+    // plays are identical
+    // rock paper
+    // rock scissors
+    // paper rock
+    // paper scissors
+    // scissors paper
+    // scissors rock
+}
