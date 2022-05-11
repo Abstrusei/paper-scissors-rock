0da45879e9222eef041426d3a5e199b868abc2d3
Sean <smarshallean@gmail.com>
2022-05-11 20:31:21 +1000

    Fixed game logic

--- psr.js
+++ psr.js
@@ -9,9 +9,9 @@ function computerPlay() {
     return computersPlay;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
      let pSelection = playerSelection.toLowerCase();
-     let cSelection = computerSelection 
+     let cSelection = computerPlay();
      let gameResults =  document.getElementById("game-results");
      let runningScore = document.getElementById("running-score");
      let finalScore = document.getElementById("final-score");
@@ -65,20 +65,6 @@ function playRound(playerSelection, computerSelection) {
    }
 }
 
-
-// Buttons
-let paperBtn = document.getElementById("paper-btn");
-let scissorsBtn = document.getElementById("scissors-btn");
-let rockBtn = document.getElementById("rock-btn");
-let refreshBtn = document.getElementById("refresh-btn");
-
-paperBtn.addEventListener('click', () => playRound("paper", computerPlay));
-scissorsBtn.addEventListener('click',  () => playRound("scissors", computerPlay));
-rockBtn.addEventListener('click',  () => playRound("rock", computerPlay));
-refreshBtn.addEventListener('click',  () =>  {
-    window.location.reload();
-});
-
 function deactivateBtns() {
     let actionBtns = document.querySelectorAll(".action-btns button");
     actionBtns.forEach(btn => {
@@ -86,6 +72,19 @@ function deactivateBtns() {
         btn.style.cssText = 'border: none';
     });
 }
+
+// Buttons
+let paperBtn = document.getElementById("paper-btn");
+let scissorsBtn = document.getElementById("scissors-btn");
+let rockBtn = document.getElementById("rock-btn");
+let refreshBtn = document.getElementById("refresh-btn");
+
+paperBtn.addEventListener('click', () => playRound("paper"));
+scissorsBtn.addEventListener('click',  () => playRound("scissors"));
+rockBtn.addEventListener('click',  () => playRound("rock"));
+refreshBtn.addEventListener('click',  () =>  {
+    window.location.reload();
+});
 
 function game() {
     // for (i = 0; i < 5; i++) {
@@ -98,9 +97,10 @@ function game() {
     // console.log("FINAL SCORE: " + "Computer: " + computerScore + ", You: " + playerScore )
 }
 
-// game();
+
+// game(); e.
 
 // For Testing
 // const playerSelection = "rock";
-// const computerSelection = computerPlay();
-// console.log(playRound(playerSelection, computerSelection));
+// const computerPLayFunc = computerPlay();
+// console.log(playRound(playerSelection, computerPLayFunc));
